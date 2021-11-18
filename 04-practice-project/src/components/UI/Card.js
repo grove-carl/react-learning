abbfca7a7395d953411390f92e815a2cbddd47d8
Xiangrui Dong <samdongxiangrui@outlook.com>
2021-11-18 14:26:55 +0800

    add Card and Button components

---
+++ 04-practice-project/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import styles from "./Card.module.css";
+
+const Card = (props) => {
+  return (
+    <div className={`${styles.card} ${props.styles}`}>{props.children}</div>
+  );
+};
+
+export default Card;
--- 04-practice-project/src/components/Users/AddUser.js
+++ 04-practice-project/src/components/Users/AddUser.js
@@ -1,18 +1,24 @@
-const AddUser = () => {
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
+import styles from "./AddUser.module.css";
+
+const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-     <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
-  )
-}
+    <Card styles={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit" onClick={addUserHandler}>Add User</Button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUser;
