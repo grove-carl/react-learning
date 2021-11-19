efaf226db91ed5c553cd0c10c5b864f79d4a85bd
Xiangrui Dong <samdongxiangrui@outlook.com>
2021-11-19 17:34:53 +0800

    add UsersList component

--- 04-practice-project/src/App.js
+++ 04-practice-project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import UsersList from './components/Users/UsersList';
 import AddUser from './components/Users/AddUser';
 
 function App() {
+  const users = [{name: 'admin', age: 23}, {name: 'guest', age: 21}];
   return (
     <div>
       <AddUser />
+      <UsersList users={users} />
     </div>
   );
 }
